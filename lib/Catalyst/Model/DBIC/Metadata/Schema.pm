1aae1bb34007c1935a86eee429025d21d3a50e49
Oliver Gorwits <oliver@cpan.org>
2009-03-28 08:00:34 +0000

    move DBIC Metadata component to its own Model namespace

--- lib/Catalyst/Model/DBIC/Metadata/Schema.pm
+++ lib/Catalyst/Model/DBIC/Metadata/Schema.pm
@@ -1,3 +1,28 @@
+package CatalystX::ListFramework::Builder::Library::DBIC::Schema;
+
+use Moose;
+use CatalystX::ListFramework::Builder::Library::Type::Primitives;
+use CatalystX::ListFramework::Builder::Library::DBIC::Source;
+
+has_boxed (
+    hash  => [qw/ sources /],
+);
+
+has 'title' => (
+    is => 'rw',
+    isa => 'Str',
+);
+
+sub add_source_metadata {
+    my ($self, $source) = @_;
+    my $s_meta = CatalystX::ListFramework::Builder::Library::DBIC::Source->new(
+        source => $source );
+    $self->sources->set($source->source_name, $s_meta);
+}
+
+no Moose;
+1;
+__END__
 package CatalystX::ListFramework::Builder::Library::Catalyst::Schema;
 
 use Moose;
