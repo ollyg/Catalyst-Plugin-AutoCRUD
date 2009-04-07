999f2a8f26907e2874cc1ba790a92f1dfc1f116b
Oliver Gorwits <oliver@cpan.org>
2009-04-07 16:26:14 +0000

    oops typo wrong chained arg

--- lib/Catalyst/Plugin/AutoCRUD/Controller/Root.pm
+++ lib/Catalyst/Plugin/AutoCRUD/Controller/Root.pm
@@ -69,7 +69,7 @@ sub source : Chained('schema') PathPart Args(1) {
     $c->stash->{template} = 'list.tt';
 }
 
-sub ajax : Chained('schema') PathPart('') CaptureArgs(1) {
+sub ajax : Chained('source') PathPart('') CaptureArgs(1) {
     my ($self, $c) = @_;
     $c->forward('do_meta');
 }
