4a58b3d452be5c2854a512b1003bc452ea966f55
Oliver Gorwits <oliver@cpan.org>
2011-08-14 12:33:42 +0100

    add is_view()

--- lib/SQL/Translator/AutoCRUD/Quick.pm
+++ lib/SQL/Translator/AutoCRUD/Quick.pm
@@ -4,8 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 
 {
-    package SQL::Translator::QuickAccess::Table;
-    use base 'SQL::Translator::Schema::Table';
+    package SQL::Translator::AutoCRUD::Quick::Source;
 
     sub new {
         my ($class, $self) = @_;
@@ -18,6 +17,18 @@ use warnings FATAL => 'all';
         $self->{cpac_f} = { map {($_->name => $_)} ($self->get_fields) };
         return $self->{cpac_f};
     }
+
+    package SQL::Translator::AutoCRUD::Quick::Table;
+    use base qw/SQL::Translator::AutoCRUD::Quick::Source
+        SQL::Translator::Schema::Table/;
+
+    sub is_view { 0 }
+
+    package SQL::Translator::AutoCRUD::Quick::View;
+    use base qw/SQL::Translator::AutoCRUD::Quick::Source
+        SQL::Translator::Schema::View/;
+
+    sub is_view { 1 }
 }
 
 use base 'SQL::Translator::Schema';
@@ -31,8 +42,10 @@ sub t {
     my $self = shift;
     return $self->{cpac_t} if $self->{cpac_t};
     $self->{cpac_t} = {
-        map {($_->name => SQL::Translator::QuickAccess::Table->new($_))}
-            ($self->get_tables, $self->get_views),
+        (map {($_->name => SQL::Translator::AutoCRUD::Quick::Table->new($_))}
+            $self->get_tables),
+        (map {($_->name => SQL::Translator::AutoCRUD::Quick::View->new($_))}
+            $self->get_views),
     };
     return $self->{cpac_t};
 }
