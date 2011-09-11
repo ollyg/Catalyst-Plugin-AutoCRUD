4245d366940bf4ba39bd44c0a0e488e7091b1a50
Oliver Gorwits <oliver@cpan.org>
2011-09-11 11:58:38 +0100

    comment out old code

--- lib/Catalyst/Plugin/AutoCRUD/Model/Backend/DBIC/Metadata.pm
+++ lib/Catalyst/Plugin/AutoCRUD/Model/Backend/DBIC/Metadata.pm
@@ -15,9 +15,6 @@ use SQL::Translator::Filter::AutoCRUD::ExtJSxType;
 use SQL::Translator::Filter::AutoCRUD::ColumnsAndPKs;
 use SQL::Translator::Filter::AutoCRUD::DisplayName;
 
-use Scalar::Util qw(weaken);
-use Carp;
-
 # return mapping of url path part to friendly display names
 # for each result source within a given schema.
 # also generate a cache of which App Model supports which source.
@@ -155,6 +152,9 @@ sub schema_metadata {
     $self->_schema_cache->{sqlt}->{$db} = $sqlt->schema;
     return $sqlt->schema;
 }
+
+#use Scalar::Util qw(weaken);
+#use Carp;
 
 #sub _build_table_info {
 #    my ($c, $cpac, $model, $tab) = @_;
@@ -363,32 +363,32 @@ sub schema_metadata {
 # is this col really part of a many to many?
 # test checks for related source having two belongs_to rels *only*,
 # and one of them refers to ourselves, and at most one other col (id pk)
-sub _ism2m {
-    my ($source, $rel) = @_;
-
-    # avoid dieing in the resence of dangling rels
-    my $fsource = eval { $source->related_source($rel) }
-        or return 0;
-    my @frels = $fsource->relationships;
-    return 0 if scalar @frels != 2 or scalar $fsource->columns > 3;
-
-    my $reverse_rel_okay = 0;
-    my $target;
-
-    foreach my $frel (@frels) {
-        return 0
-            if $fsource->relationship_info($frel)->{attrs}->{accessor} ne 'filter';
-
-        if ($fsource->related_source($frel)->source_name eq $source->source_name) {
-            $reverse_rel_okay = 1;
-        }
-        else {
-            $target = $frel;
-        }
-    }
-    return 0 if not $reverse_rel_okay;
-    return $target;
-}
+#sub _ism2m {
+#    my ($source, $rel) = @_;
+#
+#    # avoid dieing in the resence of dangling rels
+#    my $fsource = eval { $source->related_source($rel) }
+#        or return 0;
+#    my @frels = $fsource->relationships;
+#    return 0 if scalar @frels != 2 or scalar $fsource->columns > 3;
+#
+#    my $reverse_rel_okay = 0;
+#    my $target;
+#
+#    foreach my $frel (@frels) {
+#        return 0
+#            if $fsource->relationship_info($frel)->{attrs}->{accessor} ne 'filter';
+#
+#        if ($fsource->related_source($frel)->source_name eq $source->source_name) {
+#            $reverse_rel_okay = 1;
+#        }
+#        else {
+#            $target = $frel;
+#        }
+#    }
+#    return 0 if not $reverse_rel_okay;
+#    return $target;
+#}
 
 # find best table name
 sub _make_path {
@@ -401,14 +401,14 @@ sub _make_path {
 }
 
 # find best table name
-sub _rs2path {
-    my $rs = shift;
-    return $rs->from if $rs->from =~ m/^\w+$/;
-
-    my $name = $rs->source_name;
-    $name =~ s/(\w)([A-Z][a-z0-9])/$1_$2/g;
-    return lc $name;
-}
+#sub _rs2path {
+#    my $rs = shift;
+#    return $rs->from if $rs->from =~ m/^\w+$/;
+#
+#    my $name = $rs->source_name;
+#    $name =~ s/(\w)([A-Z][a-z0-9])/$1_$2/g;
+#    return lc $name;
+#}
 
 # find catalyst model serving a DBIC *result source*
 sub _find_source_model {
@@ -425,19 +425,19 @@ sub _find_source_model {
 }
 
 # find catalyst model which is serving this DBIC result source
-sub _moniker2model {
-    my ($c, $cpac, $db, $moniker) = @_;
-    my $dbmodel = $cpac->{dbpath2model}->{ $db };
-
-    foreach my $m ($c->models) {
-        my $model = eval { $c->model($m) };
-        my $test = eval { $model->result_source->source_name };
-        next if !defined $test;
-
-        return $m if $test eq $moniker and $m =~ m/^${dbmodel}::/;
-    }
-    return undef;
-}
+#sub _moniker2model {
+#    my ($c, $cpac, $db, $moniker) = @_;
+#    my $dbmodel = $cpac->{dbpath2model}->{ $db };
+#
+#    foreach my $m ($c->models) {
+#        my $model = eval { $c->model($m) };
+#        my $test = eval { $model->result_source->source_name };
+#        next if !defined $test;
+#
+#        return $m if $test eq $moniker and $m =~ m/^${dbmodel}::/;
+#    }
+#    return undef;
+#}
 
 # col/table name to human title
 sub _make_label {
@@ -445,9 +445,8 @@ sub _make_label {
 }
 
 # col/table name to human title
-sub _2title {
-    return join ' ', map ucfirst, split /[\W_]+/, lc shift;
-}
+#sub _2title {
+#    return join ' ', map ucfirst, split /[\W_]+/, lc shift;
+#}
 
 1;
-__END__
