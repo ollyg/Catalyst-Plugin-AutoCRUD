b03a4c8dae596f5a73e3509aa4a968923c8af7cb
Oliver Gorwits <oliver@cpan.org>
2011-09-13 10:28:29 +0100

    Removed View::JSON::encode_json to output Unicode.

--- lib/Catalyst/Plugin/AutoCRUD/View/JSON.pm
+++ lib/Catalyst/Plugin/AutoCRUD/View/JSON.pm
@@ -4,14 +4,6 @@ use strict;
 use warnings FATAL => 'all';
 
 use base 'Catalyst::View::JSON';
-use JSON::XS ();
-
-sub encode_json {
-    my($self, $c, $data) = @_;
-
-    my $encoder = JSON::XS->new->latin1->allow_nonref;
-    return $encoder->encode($data);
-}
 
 __PACKAGE__->config(
     expose_stash => 'json_data',
