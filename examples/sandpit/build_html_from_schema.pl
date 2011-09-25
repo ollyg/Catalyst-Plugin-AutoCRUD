63e1215e296ec428ca07c07127d1dcb3849ca839
Oliver Gorwits <oliver@cpan.org>
2011-09-25 20:40:57 +0100

    Revert "do not want sandpit"
    This reverts commit 0cd4d548c13ca6c7bba498b88c37b3af92a3cf4a.

---
+++ examples/sandpit/build_html_from_schema.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use DBI;
+use File::Temp;
+use lib './lib';
+
+use DBICTest;
+my $schema = DBICTest->init_schema(no_deploy => 1);
+
+use SQL::Translator;
+use SQL::Translator::Filter::AutoCRUD;
+
+my $t = SQL::Translator->new(
+    parser => 'SQL::Translator::Parser::DBIx::Class',
+    parser_args => { package => 'DBICTest::Schema' },
+    filters => ['SQL::Translator::Filter::AutoCRUD'],
+    producer => 'SQL::Translator::Producer::CustomHTML',
+    producer_args => { pretty => 1 },
+) or die SQL::Translator->error;
+my $out = $t->translate() or die $t->error;
+
+print $out;
