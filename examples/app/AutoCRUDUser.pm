4817fab3e2a84d74233102331b4a41000718b401
Oliver Gorwits <oliver@cpan.org>
2011-10-09 17:45:08 +0100

    delete spare demo app

--- examples/app/AutoCRUDUser.pm
+++
@@ -1,8 +0,0 @@
-package AutoCRUDUser;
-use Catalyst qw(ConfigLoader AutoCRUD);
-
-# you probably want to change the path to this file
-__PACKAGE__->config( 'Plugin::ConfigLoader' => { file => 'autocruduser.conf' } );
-
-__PACKAGE__->setup;
-1;
