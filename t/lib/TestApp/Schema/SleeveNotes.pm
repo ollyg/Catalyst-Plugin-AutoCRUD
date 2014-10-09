dd66a7a5d91c462aa8d82fd507909f76dc45f0b8
Oliver Gorwits <oliver@cpan.org>
2014-10-09 13:26:47 +0100

    [#98954] Fix malformed tests missing foreign/self prefix

--- t/lib/TestApp/Schema/SleeveNotes.pm
+++ t/lib/TestApp/Schema/SleeveNotes.pm
@@ -21,7 +21,7 @@ __PACKAGE__->add_columns(
   { data_type => "int", is_foreign_key => 1, is_nullable => 0, size => undef },
 );
 __PACKAGE__->set_primary_key("id");
-__PACKAGE__->belongs_to("album_id", "TestApp::Schema::Album", { id => "album_id" });
+__PACKAGE__->belongs_to("album_id", "TestApp::Schema::Album", { "foreign.id" => "self.album_id" });
 __PACKAGE__->add_unique_constraint(['album_id']);
 
 # Created by DBIx::Class::Schema::Loader v0.04999_05 @ 2008-08-03 20:38:57
@@ -31,7 +31,7 @@ __PACKAGE__->add_unique_constraint(['album_id']);
 __PACKAGE__->has_many(
     'nonexistent_things',
     'Nonexistent::Schema::Noggin',
-    { id => 'noggin_id' },
+    { "foreign.id" => 'self.noggin_id' },
 );
 
 # You can replace this text with custom content, and it will be preserved on regeneration
